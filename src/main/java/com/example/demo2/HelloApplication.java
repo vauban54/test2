708ec201c7dfdacd30e3f843c0a35b3f65a65891
vauban54 <vauban54@gmail.com>
2021-10-06 16:02:57 +0200

    New Changes

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -15,11 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        
-        
-        
-        
-        //fljjhsdljhgjg
     }
     
 
