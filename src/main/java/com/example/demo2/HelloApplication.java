c58ca45337b8e142f232e031684976edf21bb1f2
arnaud-henry <83584443+vauban54@users.noreply.github.com>
2021-10-06 15:58:18 +0200

    Update HelloApplication.java

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -15,7 +15,13 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        
+        
+        
+        
+        //fljjhsdljhgjg
     }
+    
 
     public static void main(String[] args) {
         launch();
